b6f3a5266a68ea2503f69d0657be677ff4482dda
KMaleesha <maleeshakumarasinghe@gmail.com>
2023-10-03 13:25:40 +0530

    SSD bug fixed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:katha/splashScreen.dart';
-
 import 'package:provider/provider.dart';
 import 'Provider/internet_provider.dart';
 import 'Provider/sign_in_provider.dart';
@@ -14,12 +12,31 @@ import 'Screens/ScreenTest/Correct.dart';
 import 'Screens/ScreenTest/InCorrect.dart';
 import 'Screens/ScreenTest/HowToSpeak.dart';
 import 'firebase_options.dart';
+import 'package:http/http.dart' as http;
+
+void fetchMetaData() async {
+  final response = await http.get(Uri.parse('http://169.254.169.254/latest/meta-data/instance-id'));
+
+  if (response.statusCode == 200) {
+    // If the server returns a 200 OK response, parse the metadata
+    print('Metadata: ${response.body}');
+  } else {
+    // If the server returns an unsuccessful response code, throw an exception.
+    throw Exception('Failed to load metadata');
+  }
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Fetch Meta Data
+  fetchMetaData();
+
+  // Initialize Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(const MyApp());
 }
 
@@ -35,9 +52,9 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider(
           create: ((context) => InternetProvider()),
-        )
+        ),
       ],
-      child:  const MaterialApp(
+      child: const MaterialApp(
         home: SplashScreen(),
         debugShowCheckedModeBanner: false,
       ),
